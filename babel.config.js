119b3de0fa81c4e2a918cbb9af34b205454ddea9
Arek N <arkadiusz.nurzynski@usertive.com>
2020-12-29 17:41:39 +0100

    chore: automatic ts paths aliasing in babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+const srcDir = path.resolve(__dirname, './src');
+
 module.exports = function(api) {
   api.cache(true);
   return {
@@ -7,19 +12,10 @@ module.exports = function(api) {
         'module-resolver',
         {
           extensions: ['.ts', '.tsx'],
-          alias: {
-            '@/ad-hocs': './src/ad-hocs',
-            '@/assets': './src/assets',
-            '@/components': './src/components',
-            '@/contexts': './src/contexts',
-            '@/exceptions': './src/exceptions',
-            '@/graphql': './src/graphql',
-            '@/hooks': './src/hooks',
-            '@/mechanisms': './src/mechanisms',
-            '@/models': './src/models',
-            '@/render-props': './src/render-props',
-            '@/utils': './src/utils]'
-          },
+          alias: fs.readdirSync(srcDir)
+            .filter((filename) => fs.statSync(path.join(srcDir, filename)).isDirectory())
+            .map((dirname) => ({key: `@/${dirname}`, value: `./src/${dirname}`}))
+            .reduce((aliases, aliasObject) => Object.assign(aliases, {[aliasObject.key]: aliasObject.value}), {}),
         },
       ],
     ],
